141ffd74368ff97741aab0ac36f363add9754355
huangmin <1773287655@qq.com>
2023-09-13 18:17:13 +0800

    Promise demo1

--- src/pages/pc/example/promise/Promise.ts
+++ src/pages/pc/example/promise/Promise.ts
@@ -1,50 +1,48 @@
 enum stateStatus {
-    Pending = 'PENDING',
-    Fulfilled = 'FULFILLED',
-    Rejected = 'REJECTED'
+  Pending = 'PENDING',
+  Fulfilled = 'FULFILLED',
+  Rejected = 'REJECTED'
 }
-type Executor<T> =  (
-    resolve: (value?: T) => void,
-    reject:(reason?:any) => void,
-    ) => void
-class HPromise<T>{
-    private state: stateStatus;
-    private value?:T;
-    private resolveCallbacks : ((value?:T) => void)[];
-    private rejectCallbacks : ((value?:T) => void)[];
+type Executor<T> = (resolve: (value?: T) => void, reject: (reason?: any) => void) => void;
+class HPromise<T> {
+  private state: stateStatus;
+  private value?: T;
+  private resolveCallbacks: ((value?: T) => void)[];
+  private rejectCallbacks: ((value?: T) => void)[];
 
-    constructor(fn: Executor<T>) {
-        this.state = stateStatus.Pending;
-        this.value = undefined;
-        this.resolveCallbacks = []
-        this.rejectCallbacks = [];
-        fn(this.resolve.bind(this), this.reject.bind(this))
+  constructor(fn: Executor<T>) {
+    this.state = stateStatus.Pending;
+    this.value = undefined;
+    this.resolveCallbacks = [];
+    this.rejectCallbacks = [];
+    fn(this.resolve.bind(this), this.reject.bind(this));
+  }
+  then(resolvecb?: (value?: T) => void, rejectedcb?: (value?: T) => void) {
+    if (this.state === stateStatus.Pending) {
+      this.resolveCallbacks.push(resolvecb!);
+      this.rejectCallbacks.push(rejectedcb!);
+    } else if (this.state === stateStatus.Fulfilled) {
+      resolvecb && resolvecb(this.value);
+    } else if (this.state === stateStatus.Rejected) {
+      rejectedcb && rejectedcb(this.value);
     }
-    then(resolvecb:(value?:T) => void, rejectedcb:(value?:T) => void) {
-        if(this.state === stateStatus.Pending) {
-            this.resolveCallbacks.push(resolvecb)
-            this.rejectCallbacks.push(rejectedcb)
-        }else if (this.state === stateStatus.Fulfilled) {
-            resolvecb(this.value)
-        }else if(this.value === stateStatus.Rejected) {
-            rejectedcb(this.value)
-        }
+  }
+  resolve(value?: T) {
+    if (this.state === stateStatus.Pending) {
+      //当状态为初始状态时，改变状态，并执行回调
+      this.state = stateStatus.Fulfilled;
+      this.value = value;
+      this.resolveCallbacks.map(cb => cb(value));
     }
-    resolve(value?:T) {
-        if(this.state ===  stateStatus.Pending){ //当状态为初始状态时，改变状态，并执行回调
-            this.state = stateStatus.Fulfilled;
-            this.value = value;
-            this.resolveCallbacks.map(cb => cb(value));
-        }
-
+  }
+  reject(value?: T) {
+    if (this.state === stateStatus.Pending) {
+      //当状态为初始状态时，改变状态，并执行回调
+      this.state = stateStatus.Rejected;
+      this.value = value;
+      this.rejectCallbacks.map(cb => cb(value));
     }
-    reject(value?:T) {
-        if(this.state === stateStatus.Pending){ //当状态为初始状态时，改变状态，并执行回调
-            this.state = stateStatus.Rejected;
-            this.value = value;
-            this.rejectCallbacks.map(cb => cb(value));
-        }
-    }
+  }
 }
 
 export default HPromise;
--- src/pages/pc/example/promise/demo1.tsx
+++ src/pages/pc/example/promise/demo1.tsx
@@ -1,13 +1,55 @@
-
+import { Button, Card, Divider } from 'antd';
+import { useRef, useEffect } from 'react';
+import myPromise from './Promise';
+import { useState } from 'react';
+import React from 'react';
 
 const Demo1 = () => {
-const fs = new FileReader();
-
-const code = fs.readAsArrayBuffer('./Promise.ts')
+  const [output, setOutput] = useState<string[]>([]);
+  useEffect(() => {
+    console.log(output);
+  }, [output]);
+  const testCode = () => {
+    setOutput([]);
+    const p1 = new myPromise((resolve, reject) => {
+      reject('hello world');
+    });
+    const p2 = new myPromise((resolve, reject) => {
+      resolve('hello world');
+    });
+    p1.then(undefined, res => {
+      setOutput(prevoutput => prevoutput.concat([`err ${res}`]));
+    });
+    p2.then(res => {
+      setOutput(prevoutput => prevoutput.concat([`success ${res}`]));
+    });
+  };
+  const TestCodeArea = () => {
     return (
-        <>
-            <pre dangerouslySetInnerHTML={{ __html: code }} />
-        </>
-    )
-}
+      <div>
+        <pre>
+          <code>{testCode.toString()}</code>
+        </pre>
+        <Divider />
+        <Button type="primary" onClick={testCode}>
+          test
+        </Button>
+        <Divider />
+      </div>
+    );
+  };
+  return (
+    <React.Fragment>
+      <Card hoverable style={{ width: '80%' }} cover={<TestCodeArea></TestCodeArea>}>
+        {output.map(item => (
+          <div key={item}>{item}</div>
+        ))}
+      </Card>
+      <pre>
+        <code>{myPromise.toString()}</code>
+      </pre>
+    </React.Fragment>
+  );
+};
+
 export default Demo1;
